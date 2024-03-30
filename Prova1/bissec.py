42b1723f1f7f2e702f3986f55f2a9557ae39300a
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2024-03-30 17:54:14 -0300

    Inclui um script principal e refatora arquivos
    As implementações dos métodos agora são apenas bibliotecas. Todo o
    código de teste está no script principal. Acho que isso ajuda a
    organizar melhor as coisas

--- Prova1/bissec.py
+++ Prova1/bissec.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # bissec.py: estima raízes pelo método da bissecção
 
-def bissec(fn, a, b, epsi):
+def bissec(fn, a, b, prec):
     """
     Aplica o método da bisecção para estimar a raiz da função
     NOTA: fn(a) * fn(b) < 0 é assumido como condição inicial!
+    Retorna a raiz aproximada e o número de iterações
     """
     # Estabelece um limite de iterações, evita loops infinitos
     n = 0
     MAX_ITERS = 1000
     x0 = (a + b) / 2  # estimativa inicial
     while n < MAX_ITERS:
+        n += 1
         # As estimativas da raiz são dadas simplesmente pelo ponto médio do
         # intervalo (a,b). A cada iteração, esse intervalo é divido na metade
         # e o processo de repete, até que se atinja a precisão desejada
@@ -19,23 +21,8 @@ def bissec(fn, a, b, epsi):
         x = (a + b) / 2  # nova estimativa
         # Cálculo do erro e teste de precisão
         erro = abs(x - x0) / x0
-        if erro < epsi:
-            return x, erro  # precisão desejada foi obtida
+        if erro < prec:
+            return x, n  # precisão desejada foi obtida
         # Do contrário, o processo continua
         x0 = x
-        n += 1
-    return x, erro  # limite de iterações atingido, retorna melhor aproximação
-
-
-def main():
-    # Exemploe de aplicação: estima sqrt(5)
-    def f(x):
-        return x * x - 5
-    res, err = bissec(f, 2, 3, 0.000000000000001)
-    print(f"Aproximação: {res} (erro = {err})")
-    from math import sqrt
-    print(f"Valor exato: {sqrt(5)}")
-
-
-if __name__ == "__main__":
-    main()
+    return x, n  # limite de iterações atingido, retorna melhor aproximação
--- Prova1/cordas.py
+++ Prova1/cordas.py
@@ -1,50 +1,27 @@
 #!/usr/bin/env python3
 # cordas.py: estima raízes pelo método das cordas
 
-def cordas(fn, x0, x1, epsi):
-    "Aplica o método das cordas para estimar a raiz da função"
+# NOTA é usada aqui uma pequena variação do método das cordas em relação ao que
+# O Lúcio passou na aula. Essa forma não assume nenhum ponto fixo, sendo
+# portanto semelhante ao método Pégaso, mas sem o artíficio mais sofisticado
+# que o Pégaso emprega com testes de sinal e etc.
+def cordas(fn, x0, x1, prec):
+    """
+    Aplica o método das cordas para estimar a raiz da função
+    Retorna a raiz aproximada e o número de iterações
+    """
     # Estabelece um limite de iterações, evita loops infinitos
     n = 0
     MAX_ITERS = 1000
     while n < MAX_ITERS:
+        n += 1
         # O método das secantes é exatamente como o método de Newton, mas
         # emprega uma aproximação para a derivada. Isso é particularmente
         # útil quando a forma analítica da função não é conhecida
         x = (x0 * fn(x1) - x1 * fn(x0)) / (fn(x1) - fn(x0))
         erro = abs(x - x1) / x1
-        if erro < epsi:
-            return x, erro  # precisão desejada foi alcançada
+        if erro < prec:
+            return x, n  # precisão desejada foi alcançada
         # Do contrário, o processo continua com novos valores para x0 e x1
         x0, x1 = x1, x
-        n += 1
-    return x, erro  # limite de iterações atingido, retorna melhor aproximação
-
-
-def aprox_sqrt(n):
-    "Usa o método das cordas para estimar raízes quadradas"
-    a, b = 1, 2
-    # Isola a raiz em um bom intervalo [a,b]
-    while not (a*a <= n <= b*b):
-        a, b = b, a + 1
-    # Talvez a raiz exata já tenha sido encontrada
-    if n == a * a:
-        return a, 0
-    if n == b * b:
-        return b, 0
-
-    # Talvez não. Nesse caso, usamos o método das cordas
-    def eqn(x):
-        return x * x - n
-    return cordas(eqn, a, b, 0.00000000000000000001)
-
-
-def main():
-    # Exemplo de aplicação: estima sqrt(5)
-    aprox, erro = aprox_sqrt(5)
-    print(f"Valor aproximado: {aprox} (erro: {erro})")
-    from math import sqrt
-    print(f"Valor exato: {sqrt(5)}")
-
-
-if __name__ == "__main__":
-    main()
+    return x, n  # limite de iterações atingido, retorna melhor aproximação
